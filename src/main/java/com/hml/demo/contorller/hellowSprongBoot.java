b85102cc3487fac159e432a05303576514dc9777
xue512 <425849716@qq.com>
2018-12-19 17:28:51 +0800

    公司

---
+++ src/main/java/com/hml/demo/contorller/hellowSprongBoot.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2018-2018, 北京圣博润高新技术股份有限公司
+ * FileName: hellowSprongBoot
+ * Author:   xue
+ * Date:     2018/12/19 14:45
+ */
+package com.hml.demo.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * @author xue
+ * @create 2018/12/19
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping(value = "/xxd")
+public class hellowSprongBoot {
+    @RequestMapping(value = "/hml")
+    public Object helloWorld(){
+        return "<h1>xxd</h1>";
+    }
+
+}
